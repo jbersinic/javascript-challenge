a28fc13ae6aac415ccc78a3effef05945542bb90
jbersinic <54554895+jbersinic@users.noreply.github.com>
2020-02-26 19:35:26 -0500

    updating JS homework code

--- UFO-level-1/assets/js/app.js
+++ UFO-level-1/assets/js/app.js
@@ -2,3 +2,48 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+data.forEach((UFOsighting) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOsighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+/// FORM ////
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+    button.on("click", function(event) {
+        tbody.html("");
+        d3.event.preventDefault() 
+// Select the input element and get the raw HTML node
+   var inputElement = d3.select("#datetime");
+
+// Get the value property of the input element
+   var inputValue = inputElement.property("value");
+
+   //console.log(inputValue);
+   
+   var filteredData = data.filter(UFOsighting => UFOsighting.datetime === inputValue);
+
+   //console.log(filteredData);
+   
+   
+   ///// PUSH THE NEW VALUES BACK TO THE FORM ////
+
+   var filtertbody = d3.select("tbody");
+
+    filteredData.forEach((UFOsighting) => {
+    var row = filtertbody.append("tr");
+    Object.entries(UFOsighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+    });
